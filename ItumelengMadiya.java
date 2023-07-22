78a2a12a48114c20d64d35a40b36d1d406e4ce7d
ItumelengMadiya <140264981+ItumelengMadiya@users.noreply.github.com>
2023-07-22 21:51:03 +0200

    Add files via upload

---
+++ ItumelengMadiya.java
@@ -0,0 +1,18 @@
+
+package za.ac.tut.itumeleng;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+
+public class ItumelengMadiya extends JFrame{
+    public ItumelengMadiya(){
+        setTitle("Itumeleng   Madiya");
+        setSize(250,300);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.CYAN);
+        setVisible(true);
+    }
+}
---
+++ NameAndSurnameApp.java
@@ -0,0 +1,14 @@
+
+package nameandsurnameapp;
+
+import za.ac.tut.itumeleng.ItumelengMadiya;
+
+
+public class NameAndSurnameApp {
+
+    public static void main(String[] args) {
+        ItumelengMadiya itu;
+        itu= new ItumelengMadiya();
+    }
+    
+}
